80e0825ee4dbacbdc378c3ac34911cebed04f13b
Chester Ong <chester.ong.ch@gmail.com>
2022-03-22 03:59:39 +0800

    init actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[macro_use]
+extern crate serde_json;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok()
+        .append_header(("Content-Type", "application/json"))
+        .body(json!({"healthy": true}).to_string())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(health))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
